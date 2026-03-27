fad64a4e17c69d467d88647c1a431b05172d4e61
JacquesLouwrens <jacqueslouwrens1@outlook.com>
2026-03-27 23:17:33 +0200

    Update main.js and policyFetcher.js with fixes

--- src/fetchers/policyFetcher.js
+++ src/fetchers/policyFetcher.js
@@ -1,6 +1,6 @@
 // src/fetchers/policyFetcher.js
 
-import { requestAsBrowser } from 'apify';
+import Apify from 'apify';
 
 /**
  * Fetch the text content of a policy page.
@@ -9,7 +9,7 @@ import { requestAsBrowser } from 'apify';
  */
 export async function fetchPolicyText(url) {
     try {
-        const response = await requestAsBrowser({ url });
+        const response = await Apify.utils.requestAsBrowser({ url });
         return response.body;
     } catch (err) {
         throw new Error(`Failed to fetch policy text from ${url}: ${err.message}`);
